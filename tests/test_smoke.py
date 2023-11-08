a854dd769e4a9b1ce2c8e362d52ff8fd3549883c
Vincent <vincent@calmcode.io>
2023-11-08 14:44:41 +0100

    add ci tests

---
+++ tests/test_smoke.py
@@ -0,0 +1,18 @@
+import pytest 
+
+from prodigy_whisper import whisper_audio_transcribe
+
+
+def test_smoke_no_segment(tmpdir):
+    # Make sure we can train without errors
+    components = whisper_audio_transcribe("xxx", "audio", model="tiny.en")
+    assert "transcript" in next(components["stream"])
+
+
+def test_smoke_segment(tmpdir):
+    # Make sure we can train without errors
+    components = whisper_audio_transcribe("xxx", "audio", model="tiny.en", segment=True)
+    for ex in components["stream"]:
+        assert "transcript" in ex
+        assert ex["meta"]["path"] == "audio/talking.mp3"
+        assert isinstance(ex["meta"]["segment_id"], int)
